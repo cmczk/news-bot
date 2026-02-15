5863536a0aa56f0df2d930b79897dd71cc5925ff
Yauheni Kuntsevich <cmaczok@gmail.com>
2026-02-15 16:47:06 +0300

    chore: add db migrations

---
+++ internal/storage/migrations/20260215133452_create_sources_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS sources(
+      id SERIAL PRIMARY KEY
+    , name VARCHAR(255) NOT NULL
+    , feed_url VARCHAR(255) NOT NULL
+
+    , created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    , updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS sources;
+-- +goose StatementEnd
